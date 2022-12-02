e5a3a864cc82d0c05457a5116039c72abd62eabc
Aleksandr <doors-75@mail.ru>
2022-12-03 02:36:22 +0300

    HomeWork_2

---
+++ work001_2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите число а: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+  Console.Write($"max = {a}");
+}
+
+if (a < b)
+{
+  Console.Write($"max = {b}");
+}
+
+if (a == b)
+{
+  Console.Write("a = b");
+}
