7b454f7a23b77f6c1f6f6c5b7b7414af31830430
Aleksandr <doors-75@mail.ru>
2022-12-05 04:41:07 +0300

    seminar_2Work_15

---
+++ work007_15/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число от 1 до 7: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 1 && num <= 5)
+{
+  Console.WriteLine("Нет");
+}
+if (num == 6 || num == 7)
+{
+  Console.WriteLine("Да");
+}
+if (num < 1 || num > 7)
+{
+  Console.WriteLine("Такого дня недели нет!");
+}
