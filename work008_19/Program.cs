9640ef016242832fe62ca84e6ee1d3a74916c3c1
Aleksandr <doors-75@mail.ru>
2022-12-07 21:57:16 +0300

    seminar_3Work_19

---
+++ work008_19/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите пятизначное число: ");
+string pul = Convert.ToString(Console.ReadLine());
+if (pul.Length == 5)
+{
+  if (pul[0] == pul[4] && pul[1] == pul[3])
+  {
+    Console.WriteLine("Является паллиндромом");
+  }
+  else
+  {
+    Console.WriteLine("Не является паллиндромом");
+  }
+}
+
+
+
