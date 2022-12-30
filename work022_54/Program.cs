c60585f3df68295f22c3325b98f3d0386aa351b7
Aleksandr <doors-75@mail.ru>
2022-12-30 12:21:37 +0300

    seminar_8_54

---
+++ work022_54/Program.cs
@@ -0,0 +1,80 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int inputNumber(string str)
+{
+  int number;
+  string text;
+  while (true)
+  {
+    System.Console.Write(str);
+    text = Console.ReadLine();
+    if (int.TryParse(text, out number))
+    {
+      break;
+    }
+    System.Console.Write("Не распознанное число, попробуйте еще раз!");
+  }
+  return number;
+}
+Random rand = new Random();
+int[,] FillAndPrintArray(int m, int n)
+{
+  int[,] array = new int[m, n];
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = rand.Next(0, 10);
+    }
+  }
+  return array;
+}
+
+void PrintArray2D(int[,] arr)// метод вывода
+{
+  for (int i = 0; i < arr.GetLength(0); i++)
+  {
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+      System.Console.Write(arr[i, j] + " ");
+    }
+    System.Console.WriteLine();
+  }
+}
+
+int m = inputNumber("Введите число m: ");//GetLength(0)
+int n = inputNumber("Введите число n: ");//GetLength(1)
+int[,] matrix = FillAndPrintArray(m, n);
+PrintArray2D(matrix);
+System.Console.WriteLine();
+OrderArrayLines(matrix);
+PrintArray2D(matrix);
+
+
+
+void OrderArrayLines(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+      {
+        if (matrix[i, k] < matrix[i, k + 1])
+        {
+          int temp = matrix[i, k + 1];
+          matrix[i, k + 1] = matrix[i, k];
+          matrix[i, k] = temp;
+        }
+      }
+    }
+  }
+}
